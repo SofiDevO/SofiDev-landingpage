49578e2d3bb9a5d1e40316103c11615b3f8f41b7
SofiDevO <angiemeow89@gmail.com>
2023-06-03 01:17:11 -0600

    Se estructura el js de DarkTheme

--- js/darkTheme.js
+++ js/darkTheme.js
@@ -1,113 +1,109 @@
 const d = document;
 /* Elementos de la pagina */
 const body = d.querySelector('body');
+
+/* ELEMENTOS DEL HEADER */
 const toggle = d.getElementById ('toggleDark');
 const logo = d.getElementById ('logo');
 const span= d.getElementById ('span');
+
+/* ELEMENTOS DEL HERO */
 const fondoHero = d.getElementById('hero')
 const titulo= d.getElementById ('titulo-saludo');
 const spanTitulo = d.getElementById('span-titulo')
 const subtitulo= d.getElementById ('subtitulo');
 const profilePic = d.getElementById('profile-pic');
+const linkOracle = d.getElementById('link-oracle');
+const linkPlatzi = d.getElementById('link-platzi');
+
+/* ELEMNTOS DEL MENU HAMBURFUESA */
+const botonHamburguesa = d.getElementById('hamburguesa')
 const acercaDeHamburguesa = d.getElementById('acercaDe-menuA');
 const portafolioHamburguesa = d.getElementById('portafolio-menuA');
 const contactoHamburguesa = d.getElementById('contacto-menuA');
 const blogHamburguesa = d.getElementById('blog-menuA');
-const botonHamburguesa = d.getElementById('hamburguesa')
-const divider1 = d.getElementById('divider1');
-const contenidoTexto = d.getElementById('textos');
-const cajaCristal = d.getElementById('caja-cristal');
-const linkOracle = d.getElementById('link-oracle');
-const linkPlatzi = d.getElementById('link-platzi');
-const acercadDeSpan = d.getElementById('span-titulo2');
-const acercaDeTitulo = d.getElementById('acercaDeTitulo');
 const panelMenu = d.getElementById('panel')
 
+/* DIVIDERES */
+const divider1 = d.getElementById('divider1');
 
+/* SECCION ACERCA DE */
+const acercaDeTitulo = d.getElementById('acercaDeTitulo');
+const acercadDeSpan = d.getElementById('span-titulo2');
+const contenidoTexto = d.getElementById('textos');
+const cajaCristal = d.getElementById('caja-cristal');
+
+
+
+/* addEventListener AL BOTON DE MODO OSCURO */
 toggle.addEventListener('click', cambiarModo);
 export  function cambiarModo(){
     this.classList.toggle('bi-lightning-charge-fill')
     if(this.classList.toggle('bi-lightning-charge')){
         /* estilos del tema claro */
-        body.style.background = 'rgb(206, 206, 206)';
-        body.style.color = 'black';
+        body.style.background = '';
+        body.style.color = '';
         body.style.transition = '2s';
-        divider1.style.fill = 'rgb(206, 206, 206)';
-        
-
-        toggle.style.filter = 'invert(100%) sepia(0%) saturate(7500%) hue-rotate(155deg) brightness(102%) contrast(100%)drop-shadow(0 0 12px #000000)';
-        
-        fondoHero.style.background = 'linear-gradient(0deg, rgba(65, 6, 138, 0.736) 20%, rgba(160, 5, 126, 0.589) 63%)';
-        
-        logo.style.filter = 'drop-shadow(0 0 11px  rgba(0,0,0) ';
+        divider1.style.fill = '';
+        toggle.style.filter = '';
+        logo.style.filter = '';
         logo.style.transition = '1s';
-        logo.style.color = 'black'
-        
-        titulo.style.color = 'black';
-        
-        subtitulo.style.color = 'black';
-        
-        spanTitulo.style.filter = 'none';
-        spanTitulo.style.color = 'white';
-        
-        span.style.filter = '';
-        span.style.color = 'white'
+        logo.style.color = ''
 
-        acercadDeSpan.style.color = 'white'
-        acercadDeSpan.style.filter = '';
+        
+        /* ELEMNTOS DE HERO */
+        fondoHero.style.background = 'linear-gradient(0deg, rgba(65, 6, 138, 0.736) 20%, rgba(160, 5, 126, 0.589) 63%)';
+        titulo.style.color = 'black';
+        spanTitulo.style.filter = '';
+        spanTitulo.style.color = 'white';
+        span.style.filter = '';
+        span.style.color = ''
+        subtitulo.style.color = '';
+        
 
         /* Elemntos del menu hamburguesa */
-        botonHamburguesa.style.filter = ' invert(0%) sepia(90%) saturate(7500%) hue-rotate(78deg) brightness(93%) contrast(93%)';
+        botonHamburguesa.style.filter = '';
         acercaDeHamburguesa.style.color = '';
         portafolioHamburguesa.style.color = '';
         contactoHamburguesa.style.color = '';
         blogHamburguesa.style.color = '';
         panelMenu.style.background = '';
         panelMenu.style.backdropFilter = '';
-
         
         /* Sección Abaout me */
         profilePic.style.filter = '';
-        cajaCristal.style.background = ' rgba( 122, 122, 122, 0.25 )';
+        cajaCristal.style.background = 'rgba( 255, 255, 255, 0.25 )';
         cajaCristal.style.boxShadow= '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )';
         contenidoTexto.style.color = ''
         linkOracle.style.color = '';
         linkPlatzi.style.color = '';
         acercaDeTitulo.style.color = ''
         acercaDeTitulo.style.filter = '';
-    
-    
+        acercadDeSpan.style.color = ''
+        acercadDeSpan.style.filter = '';
     }else{
         /* Estilos en el tema oscuro */
         body.style.color = 'rgb(199, 199, 199)';
         body.style.transition = '2s';
         body.style.background = 'rgb(7, 7, 7)';
-        
-        divider1.style.fill = 'rgb(7, 7, 7)';
-        divider1.style.transition = '2s';
-        
-        toggle.style.filter = ' invert(0%) sepia(22%) saturate(7443%) hue-rotate(110deg) brightness(91%) contrast(101%) drop-shadow(0 0 12px  rgba(0,212,255,1)';
-        
-        fondoHero.style.background = 'linear-gradient(0deg, rgba(18,0,42,1) 20%,  rgba(7, 7, 7)61%)';
-        fondoHero.style.transition = '4s';
-        
-        titulo.style.color = 'white'; 
-        titulo.style.transition = '4s';
-        
-        subtitulo.style.color = 'white';
-        
         logo.style.filter = ' drop-shadow(0 0 11px  rgba(0,212,255,1)';
         logo.style.color = 'white'
-        
-        
         span.style.color = 'rgba(0,212,255,1)';
         span.style.filter = ' drop-shadow(0 0 11px  rgba(0,212,255,1)';
+        toggle.style.filter = ' invert(0%) sepia(22%) saturate(7443%) hue-rotate(110deg) brightness(91%) contrast(101%) drop-shadow(0 0 12px  rgba(0,212,255,1)';
         
+        
+        
+
+        /* ELEMENTOS DEL HERO */
+        fondoHero.style.background = 'linear-gradient(0deg, rgba(18,0,42,1) 20%,  rgba(7, 7, 7)61%)';
+        fondoHero.style.transition = '4s';
+        titulo.style.color = 'white'; 
+        titulo.style.transition = '4s';
         spanTitulo.style.color = 'rgba(0,212,255,1)';
         spanTitulo.style.filter = ' drop-shadow(0 0 11px  rgba(0,212,255,1)';
-        
+        subtitulo.style.color = 'white';
         profilePic.style.filter = 'drop-shadow(2px -5px 16px #D608A9)';
-        
         /* Elementos del menu hamburguesa */
         acercaDeHamburguesa.style.color = 'rgba(0,212,255,1)';
         portafolioHamburguesa.style.color = 'rgba(0,212,255,1)';
@@ -117,29 +113,19 @@ export  function cambiarModo(){
         panelMenu.style.background = 'rgba( 54, 54, 54, 0.4 )';
         panelMenu.style.backdropFilter = 'blur( 10px )';
         
-
+        /* DIVIDERS */
+        divider1.style.fill = 'rgb(7, 7, 7)';
+        divider1.style.transition = '2s';
         /* Sección Abaout me */
-        cajaCristal.style.background = ' rgba( 0, 0, 0, 0.85 )  ';
-        cajaCristal.style.boxShadow= ' 0 8px 32px 0 rgba( 31, 38, 135, 0.37 )';
-        
-        contenidoTexto.style.color = 'white'
-        contenidoTexto.style.filter = 'drop-shadow(0 0 12px  black'; 
-
-        linkOracle.style.color = 'white';
-        linkPlatzi.style.color = 'white';
-        
         acercadDeSpan.style.color = 'rgba(0,212,255,1)'
         acercadDeSpan.style.filter = ' drop-shadow(0 0 12px  rgba(0,212,255,1)';
         acercaDeTitulo.style.color = 'white'
         acercaDeTitulo.style.filter = ' drop-shadow(0 0 8px  black';
+        linkOracle.style.color = '#8758FF';
+        linkPlatzi.style.color = '#8758FF';
+        contenidoTexto.style.color = 'white'
+        contenidoTexto.style.filter = 'drop-shadow(0 0 12px  black'; 
+        cajaCristal.style.background = ' rgba( 0, 0, 0, 0.85 )  ';
+        cajaCristal.style.boxShadow= ' 0 8px 32px 0 rgba( 31, 38, 135, 0.37 )';
     }
-    
-
 }
-        
-
-        
-        
-        
-        
-        
