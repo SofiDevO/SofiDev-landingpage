de5fce619af4891a0f7f8a79fe16ec3c1f20ede9
SofiDevO <angiemeow89@gmail.com>
2023-07-12 02:17:48 -0600

    Se implementa cambio de idioma de manera parcial. Se corrigen temas de resposive

--- js/language.js
+++ js/language.js
@@ -8,19 +8,29 @@ const iconEs = d.querySelector('.espanol');
 const iconMobileEn = d.querySelector('.englishMobile');
 const  iconMobileEs = d.querySelector('.espanolMobile');
 
+const  textsToChange = d.querySelectorAll("[data-section]");
+
+
+
 const changeLanguage = async (language) =>{
     const requestJson = await fetch(`../languages/${language}.json`);
     const texts = await requestJson.json()
-    console.log(texts)
-    
+    for (const textToChange of textsToChange){
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+       
+        textToChange.innerHTML=texts[section][value];
+    }
 }
 
 
+/* Ocultar iconos */
 iconEn.style.display = 'none'; 
 
 export default  toogleLanguage.addEventListener('click',(e)=>{
     changeLanguage(e.target.parentElement.dataset.language);
-})
+});
+
 iconEn.addEventListener('click',()=>{
     iconEs.style.display = 'flex';
     iconEn.style.display = 'none';
