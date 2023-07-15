30403909d15b9c2ec8b4fcfd3472277d06e39a6e
SofiDevO <angiemeow89@gmail.com>
2023-07-15 00:56:23 -0600

    Se eliminan funciones de formulario. Se volvera a construir

--- js/modules.js
+++ js/modules.js
@@ -8,14 +8,6 @@ import btnRayo from "./electricSound.js";
 
 import toogleLanguage from "./language.js";
 
-import { valida } from "./form.js";
 
-const inputs = d.querySelectorAll('input');
 
-inputs.forEach(input => {
-    input.addEventListener('blur', (input)=>{
-        valida(input.target);
-    })
-        
-    
-});
+
