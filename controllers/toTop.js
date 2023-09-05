0fc1e52abff5a1ed9c982a5601a7be169723370b
SofiDevO <angiemeow89@gmail.com>
2023-09-05 03:01:26 -0600

    Nuevo diseño, se ajusta responsive, cambian menus cabecera por menus flotantes

--- controllers/toTop.js
+++ controllers/toTop.js
@@ -1,5 +1,13 @@
 /* Boton "To top" */
 const d =document;
+const cajaToTop = d.querySelector(".to__top")
+
+/* Dar click en en la caja del btn "toTop*/
+cajaToTop.addEventListener("click", ()=>{
+	window.location.href ="#"
+})
+
+
 export  const toTop = d.querySelector(".to__top") 
 window.addEventListener("scroll",()=>{
 	if(window.pageYOffset > 500){
