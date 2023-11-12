f226ec89eff28b40d2f14f4189982c6db778a0ee
SofiDevO <angiemeow89@gmail.com>
2023-11-12 03:50:54 -0600

    :egg: New secret theme

--- controllers/electricSound.js
+++ controllers/electricSound.js
@@ -1,38 +1,34 @@
-import toggleDarkMode from "../controllers/darkTheme.js";
-
 const d = document;
 const btnDarkMode = d.querySelector("[data-toogle]");
 
-function shortCircuit() {
-    let audio = new Audio();
-    audio.src = "../assets/sounds/short_circuit.mp3";
-    audio.volume -= 0.6;
-    audio.play();
+function shortCircuit(set_in_milisec) {
+	let audio = new Audio();
+	audio.src = "../assets/sounds/short_circuit.mp3";
+	audio.volume -= 0.6;
+	audio.play();
+	setTimeout(() => { audio.pause(); }, 1900);
 }
 
-export function playSound() {
-    let audio = new Audio();
-    audio.src = "../assets/sounds/franks-electricity.mp3";
-    audio.volume -= 0.7;
-    audio.play();
+
+
+
+export default function   playSound() {
+	let audio = new Audio();
+	audio.src = "../assets/sounds/franks-electricity.mp3";
+	audio.volume -= 0.7;
+	audio.play();
 }
 
 let botonRayo = 0;
 
-function clickHandler() {
-    botonRayo++;
-
-    if (botonRayo <= 10) {
-        playSound();
-    } else {
-        shortCircuit();
-        console.log("Hola " + botonRayo + ", reproduciendo shortCircuit()");
-        // Si se han hecho 11 clics, desvincula el evento click
-        if (botonRayo >= 11) {
-            btnDarkMode.removeEventListener("click", clickHandler);
-            console.log("Evento click desvinculado");
-        }
-    }
+function clickSoun() {
+	console.log(botonRayo)
+	botonRayo++;
+	if (botonRayo <= 8) {
+		playSound();
+	} else if (botonRayo <= 18) {
+		shortCircuit();
+	}
 }
 
-btnDarkMode.addEventListener("click", clickHandler);
+btnDarkMode.addEventListener("click", clickSoun);
